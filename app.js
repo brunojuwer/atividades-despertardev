b230a0574499b098856cc7bc28145829cdb8cfe7
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-10-20 18:45:52 -0300

    Deploy new version

---
+++ app.js
@@ -0,0 +1,71 @@
+const main = document.querySelector("#main");
+const exerciciesList = document.querySelector(".exercicies-list");
+
+function handleTerminalCommand(event) {
+  event.preventDefault();
+  const command = event.target.inputText.value.trim()
+  
+  if("ls" === command) {
+    listAllExcercicies();
+    createNewForm();
+  }
+  if("clear" === command) {
+    clearTerminal()
+  }
+  if(command.startsWith("visit")) {
+    visitPage(command.replace("visit ", ""));
+  }
+  event.target.reset();
+  inputFocus();
+}
+
+function visitPage(name) {
+  exercicies.forEach(ex => {
+    if(ex.name === name.trim()) {
+      window.open(ex.link, "_blank");
+    }
+  })
+}
+
+function clearTerminal() {
+   main.innerHTML = `<form 
+   class="prompt-form" 
+   autocomplete="off"
+   onsubmit="handleTerminalCommand(event)"
+ >
+   <p>➜</p>
+   <input name="inputText" type="text" autofocus autocomplete="off">
+ </form>`
+}
+
+function listAllExcercicies() {
+  const ul = document.createElement('ul');
+  ul.classList.add('exercicies-list')
+  exercicies.forEach(exercicie => {
+    const li = document.createElement('li');
+    li.innerHTML = `<li><a class="exercicie" href="${exercicie.link}" target="_blank">${exercicie.name}</a></li>`;
+    ul.appendChild(li);
+  })
+  main.appendChild(ul);
+}
+
+function createNewForm() {
+  const newForm = document.createElement('form');
+  newForm.classList.add('prompt-form');
+  newForm.setAttribute('onsubmit', "handleTerminalCommand(event)");
+  newForm.setAttribute('autocomplete', 'off')
+  newForm.innerHTML = '<p>➜</p> <input name="inputText" type="text" autofocus autocomplete="off">';
+  main.append(newForm);
+  removeFormListner()
+}
+
+function removeFormListner() {
+  const allForms = document.querySelectorAll(".prompt-form");
+
+  allForms[allForms.length - 2].removeAttribute('onsubmit');
+  allForms[allForms.length - 2].children[1].remove()
+}
+
+function inputFocus() {
+  document.querySelector('input').focus();
+}
---
+++ exercicies.js
@@ -0,0 +1,18 @@
+const exercicies = [
+  {
+    name: 'Posicionamento CSS',
+    link: './posicionamento-css/exercicio1.html'
+  },
+  {
+    name: 'Tabelas HTML',
+    link: './tabelas-html/exercicio1.html'
+  },
+  {
+    name: 'Portifólio Github',
+    link: 'https://github.com/brunojuwer/brunojuwer'
+  },
+  {
+    name: 'Prática JS 1',
+    link: './js-pratica01/index.html'
+  },
+]
